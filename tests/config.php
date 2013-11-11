ef0d17548d914dea3ab7d37e3ea086631e6a051c
Tom Oram <tom@scl.co.uk>
2013-11-11 13:02:00 +0000

    Add license header

--- tests/config.php
+++ tests/config.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * SclZfGenericMapper (https://github.com/SCLInternet/SclZfGenericMapper)
+ *
+ * @link https://github.com/SCLInternet/SclZfGenericMapper for the canonical source repository
+ * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ */
 
 namespace SclZfGenericMapperTests;
 
